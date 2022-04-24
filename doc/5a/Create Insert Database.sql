3cb887b775ba0576b664d6a23d6f54a770d61adc
Yehuda-Binik <99425841+Yehuda-Binik@users.noreply.github.com>
2022-04-24 00:55:39 -0400

    Add files via upload

---
+++ doc/5a/Create Insert Database.sql
@@ -0,0 +1,40 @@
+﻿CREATE TABLE Energy_type (
+            Name VARCHAR(40) PRIMARY KEY,
+Environmental_unit_cost money,
+
+
+Units VARCHAR(40)
+);
+
+
+CREATE TABLE Month (
+        Name DATE PRIMARY KEY,
+        Num_days int
+);
+
+
+CREATE TABLE Meter (
+        Name Varchar(40) PRIMARY KEY,
+        Energy_type Varchar(40),
+        Units Varchar(40),
+        FOREIGN KEY (Name) REFERENCES Energy_type(Name)
+);
+
+
+CREATE TABLE Energy_type_month_cost (
+        Month_name DATE,
+        Meter_name varchar(40),
+        Total_monetary_cost money,
+        Total_environmental_cost money,
+        PRIMARY KEY (Month_name, Meter_name),
+        FOREIGN KEY (Month_name) REFERENCES MONTH(Name),
+        FOREIGN KEY (Meter_name) REFERENCES Meter(Name)
+);
+
+
+CREATE TABLE Weather_month (
+        Name DATE PRIMARY KEY,
+        Num_heat_days int,
+        Num_cool_days int,
+        FOREIGN KEY (Name) REFERENCES MONTH(Name)
+);
