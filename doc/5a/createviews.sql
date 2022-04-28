041c49e8900108fd201670b52ba2d62e65fa144c
Yehuda-Binik <ybinik1@gmail.com>
2022-04-28 00:07:44 -0400

    I edited the create views to add the units column for various views. I then made progress researching the environmental costs for the various energy types

--- doc/5a/createviews.sql
+++ doc/5a/createviews.sql
@@ -1,5 +1,5 @@
 CREATE VIEW Meter_month_cost_with_energy_type AS
-SELECT Energy_type, Month_name, Amount, Total_monetary_cost, total_environmental_cost
+SELECT Energy_type, Month_name, Amount, Units, Total_monetary_cost, total_environmental_cost
 FROM Meter_month_cost
 INNER JOIN Meter
 ON Meter_month_cost.Meter_name = Meter.name;
@@ -7,10 +7,10 @@ ON Meter_month_cost.Meter_name = Meter.name;
 DELETE FROM Meter_month_cost
 WHERE Total_monetary_cost IS NULL;
 
-CREATE VIEW Meter_month_cost_aggragated_by_energy_type (Energy_type, Month_name, Amount, Total_monetary_cost, Total_environmental_cost) AS
-SELECT Energy_type, Month_name, SUM(Amount), SUM(Total_monetary_cost), SUM(Total_environmental_cost)
+CREATE VIEW Meter_month_cost_aggragated_by_energy_type (Energy_type, Month_name, Amount, Units, Total_monetary_cost, Total_environmental_cost) AS
+SELECT Energy_type, Month_name, SUM(Amount), Units, SUM(Total_monetary_cost), SUM(Total_environmental_cost)
 FROM Meter_month_cost_with_energy_type
-GROUP BY Energy_type, Month_name
+GROUP BY Energy_type, Month_name, Units
 ORDER BY Energy_type, SUM(Amount);
 
 
