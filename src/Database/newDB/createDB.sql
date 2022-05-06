9a0e16f16170cdd242c8ce9da8fbc912e079d536
Karamh10 <44618621+Karamh10@users.noreply.github.com>
2022-05-05 22:51:59 -0400

    Add files via upload

---
+++ src/Database/newDB/createDB.sql
@@ -0,0 +1,44 @@
+CREATE TABLE Energy_type (
+		Name VARCHAR(40) PRIMARY KEY,
+		Environmental_unit_cost money,
+		Units VARCHAR(40)
+);
+
+
+CREATE TABLE Month (
+        Name DATE PRIMARY KEY,
+        Num_days int
+);
+
+
+CREATE TABLE Meter (
+        Name Varchar(40) PRIMARY KEY,
+        Energy_type Varchar(40),
+        Units Varchar(40),
+        FOREIGN KEY (Energy_type) REFERENCES Energy_type(Name)
+);
+
+
+CREATE TABLE Meter_month_cost (
+        Month_name DATE,
+        Meter_name varchar(40),
+        Amount Decimal(13,2),
+        Total_monetary_cost money,
+        Total_environmental_cost money,
+		Total_cost money,
+        PRIMARY KEY (Month_name, Meter_name),
+        FOREIGN KEY (Month_name) REFERENCES MONTH(Name),
+        FOREIGN KEY (Meter_name) REFERENCES Meter(Name)
+);
+
+
+CREATE TABLE Weather_month (
+        Name DATE PRIMARY KEY,
+        Num_heat_days int,
+        Num_cool_days int,
+        FOREIGN KEY (Name) REFERENCES MONTH(Name)
+);
+
+
+
+
---
+++ src/Database/newDB/createviews.sql
@@ -0,0 +1,16 @@
+CREATE VIEW Meter_month_cost_with_energy_type AS
+SELECT Energy_type, Month_name, Amount, Units, Total_monetary_cost, total_environmental_cost, Total_cost
+FROM Meter_month_cost
+INNER JOIN Meter
+ON Meter_month_cost.Meter_name = Meter.name;
+
+DELETE FROM Meter_month_cost
+WHERE Total_monetary_cost IS NULL;
+
+CREATE VIEW Meter_month_cost_aggragated_by_energy_type (Energy_type, Month_name, Amount, Units, Total_monetary_cost, Total_environmental_cost, Total_cost) AS
+SELECT Energy_type, Month_name, SUM(Amount), Units, SUM(Total_monetary_cost), SUM(Total_environmental_cost), SUM(Total_cost)
+FROM Meter_month_cost_with_energy_type
+GROUP BY Energy_type, Month_name, Units
+ORDER BY Energy_type, SUM(Amount);
+
+
